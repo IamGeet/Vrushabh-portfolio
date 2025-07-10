787cb5bff2fa6f519f34fa9e64a00179858c9eb3
Geeta <geetshah79@gmail.com>
2025-07-10 18:08:39 +0530

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Vrushabh Portfolio',
+  description: 'ASIC Physical Design Portfolio',
+  generator: 'ASIC Physical Design Portfolio',
 }
 
 export default function RootLayout({
